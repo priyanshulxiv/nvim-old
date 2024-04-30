b4b0e37ee5b1148625e1902ab7e9224d17741759
byPriyanshu <priyanshukatuwal@gmail.com>
2024-04-30 13:21:39 +0545

    changed indent line config and color

--- lua/core/plugin_config/indentline.lua
+++ lua/core/plugin_config/indentline.lua
@@ -1,10 +1,37 @@
 require("ibl").setup({
   indent = {
+    highlight = { "IblWhitespace" }, 
     char = "▏", 
   },
   scope = {
-    enabled = true,
     show_start = false,
     show_end = false,
+    injected_languages = true,
+    priority = 1024,
+    include = {
+      node_type = {
+        ["*"] = {
+          "arguments",
+          "block",
+          "bracket",
+          "declaration",
+          "expression_list",
+          "field",
+          "for",
+          "func_literal",
+          "function",
+          "if",
+          "import",
+          "list",
+          "return_statement",
+          "short_var_declaration",
+          "switch_body",
+          "try",
+          "type",
+        },
+      },
+    },
   },
+  vim.api.nvim_set_hl(0, "IblScope", { fg = "#fe423a" })
 })
+
