29585ba4040a59470eab0fca83030da08fd848f6
byPriyanshu <priyanshukatuwal@gmail.com>
2024-05-07 16:39:43 +0545

    removed treesitter indent disable for html and css

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -8,7 +8,7 @@ require'nvim-treesitter.configs'.setup {
   },
   indent = {
     enable = true,
-    disable = {"html", "css", "python"}
+    -- disable = {"python"}
   },
   autopairs = {
     enable = true
