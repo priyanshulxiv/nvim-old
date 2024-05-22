265682d896f9b4d203b0364c95d695b013ca4f1b
byPriyanshu <priyanshukatuwal@gmail.com>
2024-05-22 16:58:52 +0545

    updated for 0.10

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -39,4 +39,9 @@ cmp.setup({
       },
     }),
   },
+
+  -- To change the color for completion menu and diagonistics (Necessary for > Nvim 0.10)
+  vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'Normal' }),
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#2d2e33' })
 })
+
