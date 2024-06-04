8f0a37e5dee881fd2623b24875eb4698d259b252
byPriyanshu <priyanshukatuwal@gmail.com>
2024-06-04 20:58:49 +0545

    updated trouble.nvim for nvim 0.10

--- lua/core/plugin_config/trouble.lua
+++ lua/core/plugin_config/trouble.lua
@@ -1,3 +1,2 @@
-vim.keymap.set("n", "<leader>tt", ":TroubleToggle<CR>", { silent = true })
-vim.keymap.set("n", "<leader>tw", ":TroubleToggle workspace_diagnostics<CR>", { silent = true })
-vim.keymap.set("n", "<leader>td", ":TroubleToggle document_diagnostics<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tt", ":Trouble diagnostics toggle<CR>", { silent = true })
+vim.keymap.set("n", "<leader>td", ":Trouble diagnostics toggle filter.buf=0<CR>", { silent = true })
