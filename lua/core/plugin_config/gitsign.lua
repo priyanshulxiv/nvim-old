7baece2011659aaa845b87aa71c46c91d20af042
byPriyanshu <priyanshukatuwal@gmail.com>
2024-06-25 17:55:34 +0545

    removed gitsigns current line blame

--- lua/core/plugin_config/gitsign.lua
+++ lua/core/plugin_config/gitsign.lua
@@ -1,15 +1,10 @@
 require('gitsigns').setup({
-  current_line_blame = true,
-  current_line_blame_opts = {
-    delay = 100,
-    virt_text_priority = 80,
-  },
   signs = {
     add = { text = "a" },
     change = { text = "c" },
-    delete       = { text = 'd' },
-    topdelete    = { text = 'tp' },
+    delete = { text = 'd' },
+    topdelete = { text = 'tp' },
     changedelete = { text = 'cd' },
-    untracked    = { text = 'u' },
+    untracked = { text = 'u' },
   }
 })
