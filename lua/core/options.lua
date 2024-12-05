e82630a3c13e9cf32dfbfb1624c99c7d749aa347
priyanshu <priyanshukatuwal@gmail.com>
2024-12-05 17:25:45 +0545

    made pwsh as default terminal for nvim

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -33,3 +33,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.cmd([[
     autocmd BufEnter * setlocal formatoptions-=cro
 ]])
+
+-- [FOR WINDOWS] To make NeoVim use powershell as the terminal
+vim.opt.shell = "pwsh"
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = '"'
+vim.opt.shellxquote = ""
