406b17cc2bb80bb643bb8fa8d5cf0c8a5fce6030
priyanshu <priyanshukatuwal@gmail.com>
2024-12-08 19:58:37 +0545

    removed unnecessary lines in nvim-tree.lua file

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,9 +1,6 @@
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
---
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
 	sort = {
@@ -32,7 +29,6 @@ require("nvim-tree").setup({
 	filters = {
 		dotfiles = true,
 	},
-	-- This updates Nvim-Tree root directory to original everytime I go back
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
