4b05c0b79c9fd708317c58b8541f2d05249cc2d4
priyanshu <priyanshukatuwal@gmail.com>
2024-12-08 23:19:40 +0545

    some keymap changes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { silent = true })
 vim.keymap.set("n", "<leader>q", ":bd<CR>", { silent = true })
 vim.keymap.set("n", "<C-q>", ":bd!<CR>", { silent = true })
 vim.keymap.set("n", "<C-s>", ":source %<CR>", { silent = true })
-vim.keymap.set("n", "<a-z>", ":set wrap!<CR>", { silent = true }) -- Toggle Word Wrap
-vim.keymap.set("n", "<leader>m", ":setlocal nomodifiable!<CR>", { silent = true })
+vim.keymap.set("n", "<leader>lw", ":set wrap!<CR>", { silent = true }) -- Toggle Word Wrap
+vim.keymap.set("n", "<leader>ro", ":setlocal nomodifiable!<CR>", { silent = false })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -28,12 +28,13 @@ vim.keymap.set("n", "<C-d>", lazy("<C-d>zz"))
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- To exit Terminal mode into Normal mode by pressing ESC key
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 
-vim.api.nvim_set_keymap("c", "<C-j>", "<C-n>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("c", "<C-k>", "<C-p>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "gj", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "gk", { noremap = true, silent = true })
 
 vim.cmd('nnoremap <Left>  :echoe "Use h"<CR>')
 vim.cmd('nnoremap <Right> :echoe "Use l"<CR>')
--- lua/core/plugin-config/lsp-config.lua
+++ lua/core/plugin-config/lsp-config.lua
@@ -34,7 +34,7 @@ local on_attach = function(client, bufnr)
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
