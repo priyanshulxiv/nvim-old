eab1a0b4c9be9aea5e68f20cbd43465dce7dd7ac
priyanshu <priyanshukatuwal@gmail.com>
2024-12-20 20:47:37 +0545

    modified third section of statusline

--- lua/core/plugin-config/lualine.lua
+++ lua/core/plugin-config/lualine.lua
@@ -2,7 +2,15 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff" },
-		lualine_c = {},
+		lualine_c = {
+			function()
+				if vim.bo.filetype == "NvimTree" then -- Check if the current window is in nvim-tree
+					return vim.fn.fnamemodify(vim.fn.expand("%:h"), ":~") -- Show only the path (relative path)
+				else
+					return vim.fn.expand("%:t") -- Show the filename in other buffers
+				end
+			end,
+		},
 	},
 	options = {
 		show_command = false,
