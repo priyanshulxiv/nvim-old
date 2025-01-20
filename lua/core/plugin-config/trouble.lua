cbeea1f3e90b11f02f1a1e38b132f4fbf1c1e1db
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-01-20 18:30:35 +0545

    added new trouble keybinding for jumping around error list

--- lua/core/plugin-config/trouble.lua
+++ lua/core/plugin-config/trouble.lua
@@ -1,2 +1,9 @@
 vim.keymap.set("n", "<leader>tt", "<CMD>Trouble diagnostics toggle<CR>", { silent = true })
 vim.keymap.set("n", "<leader>td", "<CMD>Trouble diagnostics toggle filter.buf=0<CR>", { silent = true })
+vim.keymap.set("n", "<leader>tj", function()
+	require("trouble").next({ skip_groups = true, jump = true })
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>tk", function()
+	require("trouble").prev({ skip_groups = true, jump = true })
+end, { silent = true })
