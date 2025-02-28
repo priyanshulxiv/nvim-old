7789f796ca74c6cc623b7a80de1cd663f4e09896
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-02-28 10:22:29 +0545

    disabled telescope for big file types

--- lua/core/plugin-config/treesitter.lua
+++ lua/core/plugin-config/treesitter.lua
@@ -5,10 +5,26 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 	highlight = {
 		enable = true,
+		-- Disable treesitter highlight for large files and mentioned files
+		disable = function(lang, bufnr)
+			local langs = { "html" }
+			if vim.tbl_contains(langs, lang) then
+				return true
+			end
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 	},
 	indent = {
 		enable = true,
-		disable = { "python", "css", "javascript" },
+		-- Disable treesitter indent for files with more than 5k lines and for mentioned filetypes
+		disable = function(lang, bufnr)
+			local langs = { "python", "css", "javascript" }
+			return vim.api.nvim_buf_line_count(bufnr) > 5000 or vim.tbl_contains(langs, lang)
+		end,
 	},
 	autopairs = {
 		enable = true,
