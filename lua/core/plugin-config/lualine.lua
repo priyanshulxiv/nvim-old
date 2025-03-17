215e3dc403466ecffdfc18acb4443aecc9bb3b4e
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-03-17 11:07:49 +0545

    updated lualine

--- lua/core/plugin-config/lualine.lua
+++ lua/core/plugin-config/lualine.lua
@@ -1,18 +1,25 @@
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn", "info", "hint" },
+	symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+	colored = true,
+	always_visible = true,
+}
+
 require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff" },
-		lualine_c = {
-			function()
-				if vim.bo.filetype == "NvimTree" then -- Check if the current window is in nvim-tree
-					return vim.fn.fnamemodify(vim.fn.expand("%:h"), ":~") -- Show only the path (relative path)
-				else
-					return vim.fn.expand("%:t") -- Show the filename in other buffers
-				end
-			end,
-		},
+		lualine_b = { "branch" },
+		lualine_c = { "filename", "diff" },
+		lualine_x = { diagnostics, "encoding", { "filetype", icons_enabled = true } },
+		lualine_y = { { "location", padding = 1 } },
+		lualine_z = { "progress" },
 	},
 	options = {
-		show_command = false,
+		globalstatus = true,
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
 	},
+	extensions = { "mason", "lazy", "quickfix", "trouble" },
 })
