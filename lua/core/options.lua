f42d621e5cda1b927ab192ff367eaf315c2ab3a5
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-21 13:45:58 +0545

    completely revamped options.lua and added new options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,31 +1,37 @@
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Leader Key
 vim.g.mapleader = " "
 
+-- Enable usage of mouse
+vim.opt.mouse = "a"
+
+-- File Encoding
+vim.opt.fileencoding = "utf-8"
+
+-- Enable 24-bit RGB color (most terminals support this)
+vim.opt.termguicolors = true
+
+-- Line numbers and Relative numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Tabs and Spaces
 vim.opt.expandtab = true -- Convert tabs to spaces with '>' and '<' commands
 vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
 
-vim.opt.pumheight = 20 -- Completion window height
+-- UI
 vim.opt.signcolumn = "yes" -- Always show the sign column, otherwise it would shift the text each time
 vim.opt.cursorline = true -- Highlight the text line of the cursor
-vim.opt.smartindent = true -- Do smart autoindenting when starting a new line
-vim.opt.breakindent = true -- Wrapped line will continue visually indented
-vim.opt.linebreak = true -- Doesn't allow breaking line in the middle of a word
-vim.opt.ignorecase = true -- Searches are case-insensitive
-vim.opt.smartcase = true -- Searches are case-insensitive unless there's a capital letter
-vim.opt.swapfile = false -- Disable creating a swap file
-vim.opt.backup = false -- Disable creating backup
-vim.opt.undofile = true -- Create a undofile
-vim.opt.wrap = true -- Enable Line Wrap
 vim.opt.showcmd = true -- Enable showing (partial) commands in the last line of screen
-vim.opt.incsearch = true -- While typing a search command, highlight the first matching pattern
-vim.opt.hlsearch = true -- Highlights all occurrences of the search pattern
-vim.opt.updatetime = 100 -- Make Neovim more responsive
+vim.opt.showmode = false -- Disable showing current mode below statusline (eg: -- INSERT --)
 vim.opt.scrolloff = 8 -- Keep 8 lines above and below the cursor
-
-vim.opt.termguicolors = true
+vim.opt.sidescrolloff = 8 -- Keep 8 columns to the left and right of the cursor
+vim.opt.pumheight = 20 -- Completion window height
+vim.opt.updatetime = 300 -- Make Neovim more responsive (4000ms default)
 
 -- Cursor changes
 vim.opt.guicursor = {
@@ -34,7 +40,23 @@ vim.opt.guicursor = {
   "sm:blinkwait175-blinkoff150-blinkon175",
 }
 
--- Fold and others
+-- Windows
+vim.opt.splitbelow = true -- Force all horizontal splits to go below the current window
+vim.opt.splitright = true -- Force all vertical splits to go to the right of the current window
+
+-- Line wrappings
+vim.opt.wrap = true -- Enable Line Wrap
+vim.opt.linebreak = true -- Disable breaking line in the middle of a word
+vim.opt.breakindent = true -- Wrapped line will continue visually indented
+vim.opt.smartindent = true -- Do smart autoindenting when starting a new line
+
+-- Text search
+vim.opt.incsearch = true -- While typing a search command, highlight the first matching pattern
+vim.opt.hlsearch = true -- Highlights all occurrences of the search pattern
+vim.opt.ignorecase = true -- Searches are case-insensitive
+vim.opt.smartcase = true -- Searches are case-insensitive unless there's a capital letter
+
+-- Folds
 vim.opt.foldmethod = "manual" -- Folds are created manually
 vim.opt.foldcolumn = "1" -- Always show foldcolumn
 vim.opt.fillchars:append({
@@ -43,5 +65,8 @@ vim.opt.fillchars:append({
   foldsep = "▏",
 })
 
--- Store undofiles in home/.vim/undodir/
-vim.opt.undodir = vim.fn.expand("$HOME") .. "/.vim/undodir"
+-- VIM files
+vim.opt.swapfile = false -- Disable creating a swap file
+vim.opt.backup = false -- Disable creating backup
+vim.opt.undofile = true -- Create a undofile
+vim.opt.undodir = vim.fn.expand("$HOME") .. "/.vim/undodir" -- Store undofiles in home/.vim/undodir/
