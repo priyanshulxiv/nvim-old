23c824ddc345af6073e5b6a5e10ecef2e64a68b7
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-21 13:47:15 +0545

    terminal keymap update

--- after/ftplugin/terminal.lua
+++ after/ftplugin/terminal.lua
@@ -5,4 +5,4 @@ vim.opt_local.cursorline = false
 vim.opt_local.signcolumn = "no"
 
 -- Exit Terminal mode into Normal mode by pressing ESC key
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { buffer = true })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = true })
