556d1a6f7274cb60f78f20e7b4c9b7a7697da8d8
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-21 09:37:58 +0545

    some keymap changes and added new cool keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,29 +1,50 @@
--- Stop hlsearch
-vim.keymap.set("n", "<leader>h", "<CMD>nohlsearch<CR>", { silent = true })
-
--- Create split and move cursor to new split
-vim.keymap.set("n", "|", "<CMD>vsplit<CR><C-w>l", { silent = true })
-vim.keymap.set("n", "-", "<CMD>split<CR><C-w>j", { silent = true })
+-- <LEADER> keymaps
 
 -- Quick file save/close action
 vim.keymap.set("n", "<leader>w", "<CMD>w<CR>", { silent = true })
 vim.keymap.set("n", "<leader>q", "<CMD>bp|bd #<CR>", { silent = true })
+
+-- Stop hlsearch
+vim.keymap.set("n", "<leader>h", "<CMD>nohlsearch<CR>", { silent = true })
+
+-- Search and replace word under the cursor
+vim.keymap.set("n", "<leader>sw", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 
 -- Line Wrap
 vim.keymap.set("n", "<leader>lw", "<CMD>set wrap!<CR>", { silent = true })
 
 -- Toggle Read-Only mode for current buffer
 vim.keymap.set("n", "<leader>ro", function()
-  local modifiable = vim.bo.modifiable
-
-  vim.bo.modifiable = not modifiable
-
+  vim.bo.modifiable = not vim.bo.modifiable
   if vim.bo.modifiable then
-    vim.notify("[Modifiable]", vim.log.levels.INFO)
+    vim.notify("Modifiable.", vim.log.levels.INFO)
   else
-    vim.notify("[Read only]", vim.log.levels.WARN)
+    vim.notify("Read only.", vim.log.levels.WARN)
   end
 end, { silent = true })
+
+-- Copy to system clipboard
+vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
+
+-- Keep the deleted text in black-hole register
+vim.keymap.set({ "n", "x" }, "<leader>d", '"_d')
+
+-- Copy current directory path to clipboard
+vim.keymap.set("n", "<leader>cfp", function()
+  local path = vim.fn.expand("%:p:h")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied path to clipboard:\n" .. path, vim.log.levels.INFO)
+end, { silent = true })
+
+-- Terminal splits
+vim.keymap.set("n", "<leader>t-", "<CMD>belowright 10split | term<CR>", { silent = true })
+vim.keymap.set("n", "<leader>t|", "<CMD>botright 70vsplit | term<CR>", { silent = true })
+
+-- <OTHER> keymaps and remaps
+
+-- Create split and move cursor to new split
+vim.keymap.set("n", "|", "<CMD>vsplit<CR><C-w>l", { silent = true })
+vim.keymap.set("n", "-", "<CMD>split<CR><C-w>j", { silent = true })
 
 -- Move Lines in Visual mode
 vim.keymap.set("x", "J", ":m '>+1<CR>gv=gv")
@@ -55,32 +76,20 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("x", "<", "<gv")
 vim.keymap.set("x", ">", ">gv")
 
--- Move around in Line wrap
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+-- Move around in Line wrap and record relative jumps (10k, 10j) in jumplists
+vim.keymap.set("n", "j", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "j" or "gj"
+end, { expr = true })
+
+vim.keymap.set("n", "k", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "k" or "gk"
+end, { expr = true })
 
 -- Resize split with Arrow Keys
 vim.keymap.set("n", "<Up>", "<CMD>resize +2<CR>", { silent = true })
 vim.keymap.set("n", "<Down>", "<CMD>resize -2<CR>", { silent = true })
 vim.keymap.set("n", "<Right>", "<CMD>vertical resize +2<CR>", { silent = true })
 vim.keymap.set("n", "<Left>", "<CMD>vertical resize -2<CR>", { silent = true })
-
--- Copy to system clipboard
-vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
-
--- Keep the deleted text in black-hole register
-vim.keymap.set({ "n", "x" }, "<leader>d", '"_d')
-
--- Copy current directory path to clipboard
-vim.keymap.set(
-  "n",
-  "<leader>cfp",
-  '<CMD>let @+ = expand("%:p:h")<CR><CMD>lua print("Copied path to: " .. vim.fn.expand("%:p:h"))<CR>'
-)
-
--- Terminal splits
-vim.keymap.set("n", "<leader>t-", "<CMD>belowright 10split | term<CR>", { silent = true })
-vim.keymap.set("n", "<leader>t|", "<CMD>botright 70vsplit | term<CR>", { silent = true })
 
 -- Remap to move horizontally in command-line mode without using arrow key
 vim.keymap.set("c", "<C-h>", "<Left>")
