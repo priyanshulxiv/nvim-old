0fdc0c4e9828d1a0d33b55fba03d47030d69b236
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-22 08:15:08 +0545

    made changes to gruvbox search highlights

--- lua/core/plugin-config/colorscheme.lua
+++ lua/core/plugin-config/colorscheme.lua
@@ -1,5 +1,9 @@
 require("gruvbox").setup({
+  -- All colors are faithful to Gruvbox and taken from https://github.com/morhetz/gruvbox
   overrides = {
+    Search = { fg = "#1d2021", bg = "#d79921", reverse = false },
+    IncSearch = { fg = "#1d2021", bg = "#d65d0e", reverse = false },
+    -- CurSearch linked to IncSearch, Substitute linked to Search
     SignColumn = { bg = "NONE" },
     FoldColumn = { bg = "NONE" },
     NormalFloat = { bg = "#32302f" },
