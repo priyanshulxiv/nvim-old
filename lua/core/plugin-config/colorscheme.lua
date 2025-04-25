f1b1c61a2b7ca0897acaba5b2251537dfe676770
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-25 09:31:33 +0545

    some final gruvbox cleanups

--- lua/core/plugin-config/colorscheme.lua
+++ lua/core/plugin-config/colorscheme.lua
@@ -1,12 +1,12 @@
 require("gruvbox").setup({
-  -- All overrides are faithful to gruvbox.nvim colorscheme
+  -- All overrides are faithful to gruvbox.nvim colorscheme and taken from https://github.com/ellisonleao/gruvbox.nvim/blob/main/lua/gruvbox.lua
   overrides = {
     -- Search and Replace
     Search = { fg = "#d5c4a1", bg = "#665c54", reverse = false }, -- fg = light2, bg = dark3
-    CurSearch = { fg = "#282828", bg = "#d79921", reverse = false }, -- fg = dark0, bg = neutral_yellow
+    CurSearch = { fg = "#282828", bg = "#fabd2f", reverse = false }, -- fg = dark0, bg = bright_yellow
     IncSearch = { link = "CurSearch" },
     Substitute = { link = "CurSearch" },
-    -- -- UI/Columns
+    -- UI/Columns
     SignColumn = { bg = "NONE" },
     FoldColumn = { bg = "NONE" },
     Visual = { bg = "#504945", fg = "NONE" }, -- bg = dark2
