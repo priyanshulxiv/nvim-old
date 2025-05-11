a095aca38959bc9e723c5fedc5ee331155818d77
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-05-11 17:39:50 +0545

    fix(bufferline): remap Q,E to H,L respectively

--- lua/core/plugin-config/bufferline.lua
+++ lua/core/plugin-config/bufferline.lua
@@ -23,9 +23,9 @@ require("bufferline").setup({
 })
 
 -- Navigate Buffer
-vim.keymap.set("n", "E", "<CMD>BufferLineCycleNext<CR>", { silent = true })
-vim.keymap.set("n", "Q", "<CMD>BufferLineCyclePrev<CR>", { silent = true })
+vim.keymap.set("n", "L", "<CMD>BufferLineCycleNext<CR>", { silent = true })
+vim.keymap.set("n", "H", "<CMD>BufferLineCyclePrev<CR>", { silent = true })
 
 -- Reorder Buffers
-vim.keymap.set("n", "<leader>E", "<CMD>BufferLineMoveNext<CR>", { silent = true })
-vim.keymap.set("n", "<leader>Q", "<CMD>BufferLineMovePrev<CR>", { silent = true })
+vim.keymap.set("n", "<leader>L", "<CMD>BufferLineMoveNext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>H", "<CMD>BufferLineMovePrev<CR>", { silent = true })
