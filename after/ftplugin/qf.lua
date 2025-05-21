2e2cb92f4dd871c6e33347a621fc9bc7bd25eee7
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-05-21 20:10:59 +0545

    fix(qf): preserve active index of qf list after item deletion

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -7,9 +7,9 @@ vim.keymap.set("n", "]Q", "<CMD>cnewer<CR>", { silent = true })
 
 -- Remove entries from the quickfix(qf) list
 local function remove_qf_items()
-  -- Get current qf list items and title
-  local qf = vim.fn.getqflist({ items = true, title = true })
-  local items, title = qf.items, qf.title
+  -- Get current qf list's items/entries, title and index
+  local qf = vim.fn.getqflist({ items = true, title = true, idx = 0 })
+  local items, title, current_idx = qf.items, qf.title, qf.idx
   -- Get current mode
   local mode = vim.fn.mode()
   -- Item(s) selection range
@@ -29,6 +29,9 @@ local function remove_qf_items()
   start_line = math.max(1, math.min(start_line, #items))
   end_line = math.max(1, math.min(end_line, #items))
 
+  -- Calculate no. of items deleted
+  local delete_count = end_line - start_line + 1
+
   -- Remove qf items in reverse order to avoid index shifting issues
   for i = end_line, start_line, -1 do
     table.remove(items, i)
@@ -39,6 +42,17 @@ local function remove_qf_items()
 
   -- Prevent cursor going out-of-bounds and close qf window if all items are deleted
   if #items > 0 then
+    local new_idx = current_idx
+    -- If active qf entry was among the deleted items range, set the index to first item of the qf list
+    if current_idx >= start_line and current_idx <= end_line then
+      new_idx = 1
+    -- If active qf entry was below the deleted items range then do some calculation to preserve the active qf entry
+    elseif current_idx > end_line then
+      new_idx = math.max(1, current_idx - delete_count)
+    end
+    -- Set the correct active entry for the qf
+    vim.fn.setqflist({}, "r", { idx = new_idx })
+    -- Cursor position in the updated qf list
     vim.api.nvim_win_set_cursor(0, { math.min(start_line, #items), 0 })
   else
     vim.cmd("cclose")
