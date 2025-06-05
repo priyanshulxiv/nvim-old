a9e6f26ef5027c0f5fab060e7c1fddc6f8039bed
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-06-05 19:59:35 +0545

    feat: add fuzzy recursive path for commands like :find and others

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -56,6 +56,9 @@ vim.opt.hlsearch = true -- Highlights all occurrences of the search pattern
 vim.opt.ignorecase = true -- Searches are case-insensitive
 vim.opt.smartcase = true -- Searches are case-insensitive unless there's a capital letter
 
+-- Fuzzy Find
+vim.opt.path = "*,,**"
+
 -- Grep
 vim.opt.grepprg = "rg --vimgrep"
 vim.opt.grepformat = "%f:%l:%c:%m"
